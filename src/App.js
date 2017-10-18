dd6bb237e27422cc8d2af509e3cd66ef40348b1d
Aamish Baloch <aamish.baloch@gmail.com>
2017-10-18 18:14:55 +0500

    added todo list through states

--- src/App.js
+++ src/App.js
@@ -3,28 +3,40 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React TODO App</h1>
-        </header>
-        <div className="todo-app">
+    constructor(){
+        super()
+        this.state = {
+            todos: [
+                {id: 1, name: "Learn React", completed: true},
+                {id: 2, name: "Learn Python", completed: false},
+                {id: 3, name: "Learn Big Data", completed: false}
+            ]
+        }
+    }
+    render() {
+        return (
+          <div className="App">
+            <header className="App-header">
+              <img src={logo} className="App-logo" alt="logo" />
+              <h1 className="App-title">Welcome to React TODO App</h1>
+            </header>
+            <div className="todo-app">
 
-            <input type="text"/>
+                <input type="text"/>
 
-            <div className="todo-list">
-                <ul>
-                    <li><input type="checkbox"/>Learn React</li>
-                    <li><input type="checkbox"/>Learn Python</li>
-                    <li><input type="checkbox"/>Deploy App</li>
-                </ul>
+                <div className="todo-list">
+                    <ul>
+                        {this.state.todos.map(todo =>
+                            <li key={todo.id}>
+                                <input type="checkbox" defaultChecked={todo.completed}/>{todo.name}
+                            </li>
+                        )}
+                    </ul>
+                </div>
             </div>
-        </div>
-      </div>
-    );
-  }
+          </div>
+        );
+    }
 }
 
 export default App;
