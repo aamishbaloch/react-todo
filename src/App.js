6899a215fd4100d2eea9cf81f56619bf07d30459
Aamish Baloch <aamish.baloch@gmail.com>
2017-10-18 17:59:49 +0500

    todo list added

--- src/App.js
+++ src/App.js
@@ -8,11 +8,20 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to React TODO App</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="todo-app">
+
+            <input type="text"/>
+
+            <div className="todo-list">
+                <ul>
+                    <li><input type="checkbox"/>Learn React</li>
+                    <li><input type="checkbox"/>Learn Python</li>
+                    <li><input type="checkbox"/>Deploy App</li>
+                </ul>
+            </div>
+        </div>
       </div>
     );
   }
